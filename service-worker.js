fee847947767703575bedb1a4973f71d6584acc0
lassenedreberg-glitch <lassenedreberg@hotmail.com>
2026-09-01 18:55:26 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="jegerproven-v25";
+const CACHE="jegerproven-v26";
 const FILES=["./","./index.html","./questions.js","./questions-2.js","./questions-3.js","./weapon-diagrams.js","./avatar-builder.js","./i18n.js","./glossary.js","./shop.js","./shop-icons.svg","./multiplayer.js","./multiplayer.css","./clay-game.js","./clay-game.css","./manifest.json","./jerven-icon.png","./icon-192.png","./icon-512.png","./game-background.png","./gutt-jerven.png","./jente-jerven.png"];
 
 self.addEventListener("install",event=>{
@@ -13,5 +13,10 @@ self.addEventListener("activate",event=>{
 
 self.addEventListener("fetch",event=>{
   if(event.request.method!=="GET")return;
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
+  const request=event.request;
+  if(request.mode==="navigate"||new URL(request.url).pathname.endsWith("/index.html")){
+    event.respondWith(fetch(request,{cache:"no-store"}).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put("./index.html",copy));return response}).catch(()=>caches.match("./index.html")));
+    return;
+  }
+  event.respondWith(caches.match(request).then(cached=>cached||fetch(request)));
 });
