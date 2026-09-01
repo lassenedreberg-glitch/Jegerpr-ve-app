89139e65edcc67d6017e6f4f92a75b1dfb1014f7
lassenedreberg-glitch <lassenedreberg@hotmail.com>
2026-09-01 06:59:13 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+const CACHE="jegerproven-v2";
+const FILES=["./","./index.html","./questions.js","./manifest.json","./jerven-icon.png","./icon-192.png","./icon-512.png","./game-background.png","./lukas-jerven.png","./jente-jerven.png"];
+
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)));
+});
